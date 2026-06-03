22b0ff8d489b7e4d924b3c5889d867cac949df1d
Misbah Bazina <bazinamisbah@gmail.com>
2026-06-03 17:37:48 +0300

    Bazina's Portfolio

---
+++ projects.js
@@ -0,0 +1,7 @@
+const projects = [
+  {
+    name: "NumericalAnalysis Solver",
+    description: "Solves numerical problems using bisection and other methods, showing step-by-step solutions. Also solves matrix equations and returns the value of each variable.",
+    githubUrl: "https://github.com/Bazzzina/NumericalWeb"
+  }
+];
